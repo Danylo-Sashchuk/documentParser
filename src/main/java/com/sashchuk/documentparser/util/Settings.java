93fa8b62bc9bd13e88fe6c90db650a4efd93263b
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-01 22:30:25 -0400

    Unit tests for settings

--- src/main/java/com/sashchuk/documentparser/util/Settings.java
+++ src/main/java/com/sashchuk/documentparser/util/Settings.java
@@ -1,12 +1,14 @@
 package com.sashchuk.documentparser.util;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 
 public class Settings {
-    public static final String TESSETACT_DATAPATH = "/opt/homebrew/Cellar/tesseract/5.3.1/share/tessdata";
-    public static final String VM_ARGUMENTS = "-Djna.library.path=/opt/homebrew/Cellar/tesseract/5.3.1/lib";
+    //public static final String TESSETACT_DATAPATH = "/opt/homebrew/Cellar/tesseract/5.3.1/share/tessdata";
+//    public static final String VM_ARGUMENTS = "-Djna.library.path=/opt/homebrew/Cellar/tesseract/5.3.1/lib";
 
 
     private static Settings instance;
@@ -18,16 +20,21 @@ public class Settings {
 
     private Settings(String settingsPath) {
         propertiesInstance = new Properties();
-        Path path = Paths.get(Path.of(settingsPath, "settings").toUri());
-        System.out.println(path);
-        System.out.println("break");
+        Path path = Paths.get(settingsPath, "settings");
+        try {
+            propertiesInstance.load(Files.newBufferedReader(path));
+        } catch (IOException ioException) {
+            throw new RuntimeException("settings files has not been read");
+        }
     }
 
     public static String getProperty(String key) {
+        getInstance();
         return propertiesInstance.getProperty(key);
     }
 
     private static void setProperty(String key, String value) {
+        getInstance();
         propertiesInstance.setProperty(key, value);
     }
 
--- src/test/java/com/sashchuk/documentparser/util/SettingsTest.java
+++ src/test/java/com/sashchuk/documentparser/util/SettingsTest.java
@@ -1,22 +1,33 @@
 package com.sashchuk.documentparser.util;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SettingsTest {
-
-    @Test
-    void getProperty() {
-
+    static Stream<Arguments> getProperties() {
+        return Stream.of(
+                arguments("ENABLE_FEATURE_A", "true"),
+                arguments("ENABLE_FEATURE_B", "false"),
+                arguments("API_KEY", "1234567890"),
+                arguments("DATABASE_HOST", "localhost"),
+                arguments("DATABASE_PORT", "5432"));
     }
 
-    @Test
-    void getInstance() {
-        Settings.getInstance();
-        Assertions.assertEquals(1, 2);
+    @BeforeEach
+    void setUp() {
+        Settings.getInstance("/Users/Danylo/Desktop/Workspace/documentparser/src/test/resources");
     }
 
-    @Test
-    void testGetInstance() {
+    @ParameterizedTest
+    @MethodSource
+    void getProperties(String key, String value) {
+        Assertions.assertThat(value).isEqualTo(Settings.getProperty(key));
     }
 }
