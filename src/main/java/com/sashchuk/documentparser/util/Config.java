35f5140b3c33d1d4377c0fb46d962b3a50fc805a
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-09 21:48:43 -0400

    Documentation for Config file

--- src/main/java/com/sashchuk/documentparser/util/Config.java
+++ src/main/java/com/sashchuk/documentparser/util/Config.java
@@ -8,6 +8,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+/**
+ * This is a config singleton that contains all the necessary configs through the {@link java.util.Properties} class.
+ */
+
+
 public class Config {
     public static final String DEFAULT_SETTINGS_PATH = "src/main/resources";
     private static final Config instance = new Config();
@@ -21,6 +26,11 @@ public class Config {
         loadSettings(settingsPath);
     }
 
+    /**
+     * @param key key of the property.
+     * @return value of the property.
+     * @throws ConfigException if property does not exist.
+     */
     public static String getProperty(String key) {
         String property = propertiesInstance.getProperty(key);
         if (property == null) {
@@ -29,6 +39,11 @@ public class Config {
         return property;
     }
 
+    /**
+     * @param key   key of the property to store.
+     * @param value value of the property to store.
+     * @throws ConfigException if property already exists.
+     */
     public static void setProperty(String key, String value) {
         if (propertiesInstance.containsKey(key)) {
             throw new ConfigException("Property " + key + " already exists");
@@ -36,20 +51,36 @@ public class Config {
         propertiesInstance.setProperty(key, value);
     }
 
+    /**
+     *
+     * @return new HashMap&lt;String, String&gt; with all the properties of the config.
+     */
     public static Map<String, String> getProperties() {
         Map<String, String> properties = new HashMap<>();
         propertiesInstance.forEach((key, value) -> properties.put((String) key, (String) value));
         return properties;
     }
 
+    /**
+     * Reloads the config.
+     */
     public static void recreate() {
         loadSettings(DEFAULT_SETTINGS_PATH);
     }
 
+    /**
+     * Reloads the config from the specified path.
+     * @param path path do the config's folder.
+     */
     public static void recreate(String path) {
         loadSettings(path);
     }
 
+    /**
+     * Loads the config from the specified path.
+     * @param settingsPath path do the config's folder.
+     * @throws ConfigException if exception occurs while loading the config file.
+     */
     private static void loadSettings(String settingsPath) {
         propertiesInstance = new Properties();
         Path path = Paths.get(settingsPath, "settings");
--- src/test/java/com/sashchuk/documentparser/tesseract/TesseractTest.java
+++ src/test/java/com/sashchuk/documentparser/tesseract/TesseractTest.java
@@ -27,19 +27,17 @@ public class TesseractTest {
         File file = new File("src/test/resources/ProcessEssay.pdf");
         String expected = """
                 Write Your Title Here
-                After outlining about the topic “How to be a successful college student,” start your paragraph by indenting it. Then, continue with your topic sentence defis remember to include the topic “successful student” and the controlling idea “steps” in your topic sentence. After you write your TS, provide the first supporting detail, which should be the first important step. Then provide the two minors for your SD1. Remember to provide an example and explanation for each minor. Continue your paragraph following the structure learned in class.
+                                
+                After outlining about the topic “How to be a successful college student,” start your
+                paragraph by indenting it. Then, continue with your topic sentence defis remember to include the
+                topic “successful student” and the controlling idea “steps” in your topic sentence. After you write
+                your TS, provide the first supporting detail, which should be the first important step. Then
+                provide the two minors for your SD1. Remember to provide an example and explanation for each
+                minor. Continue your paragraph following the structure learned in class.
                 """;
 
-        //When
         String actual = tesseract.doOCR(file);
-        expected = expected.replace("\n", " ")
-                .replace("\r", " ")
-                .replace(" ", "");
-        actual = actual.replace("\n", "")
-                .replace("\r", "")
-                .replace(" ", "");
 
-        //Then
         Assertions.assertThat(expected)
                 .isEqualTo(actual);
     }
